99c66ddc55eac119f7d6dff0fbefd544cf94d9e4
nomanwaris <nomanwaris0347@gmail.com>
2019-02-17 19:44:53 +0500

    answers posted

--- server/server.js
+++ server/server.js
@@ -1,41 +1,41 @@
 var express = require('Express');
 var bodyParser = require('body-parser');
 
-var {mongoose} = require('./db/mongoose');
-var {User}=require('./models/user');
-var {Question} = require('./models/question');
+var { mongoose } = require('./db/mongoose');
+var { User } = require('./models/user');
+var { Question } = require('./models/question');
 
-var app= express();
+var app = express();
 
 app.use(bodyParser.json());
 
-app.use(function (req, res, next) {        
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');    
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');    
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');      
-    res.setHeader('Access-Control-Allow-Credentials', true);       
-    next();  
-}); 
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
 
 //angular (public) files
 //app.use(express.static(__dirname + '/../public')); 
 
 
 //method for addtion of contact
-app.post('/user',(req,res)=>{
+app.post('/user', (req, res) => {
     //mongoose object instance
     var user = new User({
-        first_name:  req.body.first_name,
+        first_name: req.body.first_name,
         last_name: req.body.last_name,
-        email:   req.body.email,
-        username : req.body.username,
-        password : req.body.password,
+        email: req.body.email,
+        username: req.body.username,
+        password: req.body.password,
     });
     //saving contact to db
-    user.save().then((doc)=>{
-            //returning new object to user
-            res.status(200).send(doc);
-    },(err)=>{
+    user.save().then((doc) => {
+        //returning new object to user
+        res.status(200).send(doc);
+    }, (err) => {
         res.status(400).send(err);
     })
 });
@@ -51,59 +51,86 @@ app.post('/user',(req,res)=>{
 // })
 // });
 //User login authentication
-app.post('/user/auth',(req,res)=>{
+app.post('/user/auth', (req, res) => {
     //mongoose method for deleting object from db
-//console.log(req.body.email);
-console.log(req.body.password);
-console.log(req.body.email);
-    User.find({"email":req.body.email,"password":req.body.password}).then((doc)=>{
+    //console.log(req.body.email);
+    console.log(req.body.password);
+    console.log(req.body.email);
+    User.find({ "email": req.body.email, "password": req.body.password }).then((doc) => {
         console.log(doc);
         res.send(doc);
-},(err)=>{
-    res.status(400).send(err);
-})
+    }, (err) => {
+        res.status(400).send(err);
+    })
 });
 //method for fetching questions
-app.post('/questions',(req,res)=>{
+app.post('/questions', (req, res) => {
 
     //console.log('asdasd')
-    
-    Question.find({}).then((doc)=>{
-        console.log(doc);
-        res.send(doc);
-},(err)=>{
-    res.status(400).send(err);
-})
+    let arr = [];
+    Question.find({}).then((doc) => {
+
+        doc.forEach(element => {
+            User.findOne({ '_id': element.user_id }).then((user) => {
+                arr.push({ question: element, user: { 'username': user.username } })
+                if (doc.length == arr.length) {
+                    res.send(arr);
+                }
+            }), (err) => {
+                res.status(400).send(err);
+            }
+        })
+
+    }, (err) => {
+        res.status(400).send(err);
+    })
 });
 //method for fetching answers
-app.post('/answers',(req,res)=>{
+app.post('/answers', (req, res) => {
 
     //console.log('asdasd')
     //console.log(req.body.questionID);
-    Question.find({'_id':req.body.questionID}).then((doc)=>{
+    Question.find({ '_id': req.body.questionID }).then((doc) => {
         console.log();
         res.send(doc);
-},(err)=>{
-    res.status(400).send(err);
-})
+    }, (err) => {
+        res.status(400).send(err);
+    })
 });
 
 //method for posting question
-app.post('/postquestion',(req,res)=>{
+app.post('/postquestion', (req, res) => {
 
     //console.log('asdasd')
     //console.log(req.body.questionID);
     var question = new Question({
-        question :  req.body.question,
-        user_id : '5c095910aca26c1254be01ff',
-        category :   req.body.category
-        
+        question: req.body.question,
+        user_id: '5c095910aca26c1254be01ff',
+        category: req.body.category
+
     });
     //saving contact to db
-    question.save().then((doc)=>{
-            //returning new object to user
-            res.status(200).send(doc);
-    },(err)=>{
+    question.save().then((doc) => {
+        //returning new object to user
+        res.status(200).send(doc);
+    }, (err) => {
+        res.status(400).send(err);
+    })
+});
+
+//method for posting answers
+app.post('/postanswer', (req, res) => {
+
+    //console.log('asdasd')
+    //console.log(req.body.questionID);
+    //saving answer to db
+    Question.updateOne(
+        { '_id': req.body.questionID },
+        { $push: { answer: { answer: req.body.answer, user_id: req.body.questionID, rating: { approved: false, score: 0 } } } },
+        { returnOriginal: false }
+    ).then((doc) => {
+        res.send(doc);
+    }, (err) => {
         res.status(400).send(err);
     })
 });
@@ -145,6 +172,6 @@ app.post('/postquestion',(req,res)=>{
 // });
 
 
-app.listen(3000,()=>{
+app.listen(3000, () => {
     console.log('Server started on 3000')
 })
